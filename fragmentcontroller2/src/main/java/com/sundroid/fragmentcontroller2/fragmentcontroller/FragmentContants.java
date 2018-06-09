46d76b92d23972dfb1c028dd7c6edda2d264ec97
vishwakarmasunil68 <vishwakarmasunil68@gmail.com>
2018-06-09 11:12:56 +0530

    Initial commit

---
+++ fragmentcontroller2/src/main/java/com/sundroid/fragmentcontroller2/fragmentcontroller/FragmentContants.java
@@ -0,0 +1,10 @@
+package com.sundroid.fragmentcontroller2.fragmentcontroller;
+
+public class FragmentContants {
+    public static final int RESULT_OK=1;
+    public static final int RESULT_CANCELED=0;
+
+    public static int add(int x , int y){
+        return x+y;
+    }
+}
